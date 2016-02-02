116009271903e36107b3d82db6c4181ef5184af0
kvokka <root_p@mail.ru>
2016-02-02 10:16:16 +0100

    Init commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,23 @@
+require 'application_responder'
+
+class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+  respond_to :html
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  # v.3.5 syntax. will be deprecated in 4.0
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_in) do |user_params|
+      user_params.permit(:email, :password)
+    end
+
+    devise_parameter_sanitizer.for(:sign_up) do |user_params|
+      user_params.permit(:email, :password, :password_confirmation)
+    end
+  end
+  protected :configure_permitted_parameters
+end
